616532f5cc33c13f71c6df62a539603a888e7813
renan <renansouza0122@gmail.com>
2025-06-19 00:56:24 -0400

    feat:add card client style

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { AnnotationComponent } from './components/annotation/annotation.component';
+import { CardClientComponent } from './components/card-client/card-client.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,AnnotationComponent],
+  imports: [RouterOutlet,AnnotationComponent,CardClientComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/components/card-client/card-client.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card-client',
+  imports: [],
+  templateUrl: './card-client.component.html',
+  styleUrl: './card-client.component.scss'
+})
+export class CardClientComponent {
+
+}
