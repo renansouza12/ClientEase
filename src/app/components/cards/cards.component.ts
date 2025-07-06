96d1fc2191b3f8385cdc566b709c82f32d0d1bed
RenanSouza <renansouza0122@gmail.com>
2025-07-06 00:50:17 -0300

    feat: implement drag-and-drop reordering for clients cards

--- src/app/components/cards/cards.component.ts
+++ src/app/components/cards/cards.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { CardClientComponent } from '../card-client/card-client.component';
+import {CdkDrag,CdkDragDrop,CdkDropList,moveItemInArray} from '@angular/cdk/drag-drop';
 
   interface Client{
       name:string;
@@ -10,7 +11,7 @@ import { CardClientComponent } from '../card-client/card-client.component';
 
 @Component({
   selector: 'app-cards',
-  imports: [CardClientComponent],
+  imports: [CardClientComponent,CdkDrag,CdkDropList],
   templateUrl: './cards.component.html',
   styleUrl: './cards.component.scss'
 })
@@ -28,7 +29,59 @@ export class CardsComponent {
       plan:"Normal Plano 100R$",
       startDate:new Date("2025-07-10"),
       endDate:new Date("2025-08-10")
+    },
+    {
+      name:"Charlie",
+      plan:"Normal Plano 100R$",
+      startDate:new Date("2025-07-15"),
+      endDate:new Date("2025-08-15")
+    },
+    {
+      name:"David",
+      plan:"Family Plano 150R$",
+      startDate:new Date("2025-07-20"),
+      endDate:new Date("2025-08-20")
+    },
+    {
+      name:"Eve",
+      plan:"Normal Plano 100R$",
+      startDate:new Date("2025-07-25"),
+      endDate:new Date("2025-08-25")
+    },
+    {
+      name:"Frank",
+      plan:"Family Plano 150R$",
+      startDate:new Date("2025-07-30"),
+      endDate:new Date("2025-08-30")
+    },
+    {
+      name:"Grace",
+      plan:"Normal Plano 100R$",
+      startDate:new Date("2025-08-05"),
+      endDate:new Date("2025-09-05")
+    },
+    {
+      name:"Heidi",
+      plan:"Family Plano 150R$",
+      startDate:new Date("2025-08-10"),
+      endDate:new Date("2025-09-10")
+    },
+    {
+      name:"Ivan",
+      plan:"Normal Plano 100R$",
+      startDate:new Date("2025-08-15"),
+      endDate:new Date("2025-09-15")
+    },
+    {
+      name:"Judy",
+      plan:"Family Plano 150R$",
+      startDate:new Date("2025-08-20"),
+      endDate:new Date("2025-09-20")
     }
   ];
+
+   drop(event: CdkDragDrop<Client[]>) {
+    moveItemInArray(this.clients, event.previousIndex, event.currentIndex);
+  }
 
 }
