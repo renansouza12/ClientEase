a760fec946c784ff6cc4667c23355a1a9f7bc8cb
renan <renansouza0122@gmail.com>
2025-08-01 01:06:09 -0400

    wip: adding input validations  and erro messages

--- src/app/components/form-client/form-client.component.ts
+++ src/app/components/form-client/form-client.component.ts
@@ -1,20 +1,49 @@
 import { Component, inject } from '@angular/core';
+import { ControlValueAccessor, FormBuilder, FormControl, FormGroup, NgControl, ReactiveFormsModule, Validators } from '@angular/forms';
 import { ClientService } from '../../services/client.service';
+
+
+
 
 @Component({
   selector: 'app-form-client',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './form-client.component.html',
   styleUrl: './form-client.component.scss'
 })
-export class FormClientComponent {
+export class FormClientComponent  {
 
   private service = inject(ClientService);
 
+  private phoneRegex = /^\+?(\d{1,3})?[-.\s]?(\(?\d{2,3}\)?)[-.\s]?(\d{4,5})[-.\s]?(\d{4})$/;
 
-  protected closeForm():void{
+  protected form = new FormGroup({
+    name: new FormControl<string>('',{
+      nonNullable: true,
+        validators: [Validators.required]
+    }),
+    plan: new FormControl<string>('',{
+      nonNullable: true,
+      validators: [Validators.required]
+    }),
+    start_date: new FormControl<Date | null>(null, {
+      nonNullable: true,
+      validators: [Validators.required]
+    }),
+    end_date: new FormControl<Date | null>(null, {
+      nonNullable: true,
+      validators: [Validators.required]
+    }),
+    phone: new FormControl<string>('',{
+      nonNullable:true,
+      validators: [Validators.required, Validators.pattern(this.phoneRegex)]
+    })
+
+
+  })
+
+  protected closeForm(): void {
     this.service.hideForm();
   }
-
 
 }
