90dabdb23c71040d20e8d4ad9f53600ab2a424c3
RenanSouza <renansouza0122@gmail.com>
2025-08-28 22:48:30 -0300

    wip: adjust gap to links and snippet component

--- src/app/components/snippet-card/snippet-card.component.ts
+++ src/app/components/snippet-card/snippet-card.component.ts
@@ -1,44 +1,44 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import {CdkDrag ,CdkDragHandle } from '@angular/cdk/drag-drop';
+import { CdkDrag, CdkDragHandle } from '@angular/cdk/drag-drop';
 @Component({
   selector: 'app-snippet-card',
-  imports: [FormsModule, CommonModule, CdkDragHandle,CdkDrag],
+  imports: [FormsModule, CommonModule, CdkDragHandle, CdkDrag],
   templateUrl: './snippet-card.component.html',
   styleUrl: './snippet-card.component.scss',
-  host:{'[class.expand]':'isExpanded'} 
+  host: { '[class.expand]': 'isExpanded' }
 })
 export class SnippetCardComponent {
 
-  @Input() snippet!:number;
+  @Input() snippet!: number;
 
-    protected snippetText!:string;
-    protected notificationText:string ="Notification message";
+  protected snippetText!: string;
+  protected notificationText: string = "Notification message";
 
-    protected isExpanded: boolean = false;
-    protected showNotification: boolean = false;
+  protected isExpanded: boolean = false;
+  protected showNotification: boolean = false;
 
-    protected snippetClick():void{
-      const text = this.snippetText;
-      const urlPattern =/^(https?:\/\/[^\s/$.?#].[^\s]*)$/i;
+  protected snippetClick(): void {
+    const text = this.snippetText;
+    const urlPattern = /^(https?:\/\/[^\s/$.?#].[^\s]*)$/i;
 
-      if(urlPattern.test(text)){
-        window.open(text,'_blank');
-      }else if(text){
-        navigator.clipboard.writeText(text)
-        .then(()=>{
+    if (urlPattern.test(text)) {
+      window.open(text, '_blank');
+    } else if (text) {
+      navigator.clipboard.writeText(text)
+        .then(() => {
           this.notificationText = "Text copied to clipboard!";
           this.showNotification = true;
-          setTimeout(() =>{
+          setTimeout(() => {
             this.showNotification = false
-          },2000)
+          }, 2000)
         })
-        .catch(err => console.error('Could not copy text:',err))
-      }
+        .catch(err => console.error('Could not copy text:', err))
     }
-    protected expandTextArea(): void {
-      this.isExpanded = !this.isExpanded;
-    }
+  }
+  protected expandTextArea(): void {
+    this.isExpanded = !this.isExpanded;
+  }
 
 }
